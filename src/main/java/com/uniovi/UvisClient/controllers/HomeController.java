f5938fd5ceaa3632465bca7891fe56a495f7cab8
PelayoDiaz <hector.diaz@seresco.es>
2019-06-17 11:08:47 +0200

    Correción #11
    Corregido bug que machacaba los utxos asociados a transacciones pendientes

--- src/main/java/com/uniovi/UvisClient/controllers/HomeController.java
+++ src/main/java/com/uniovi/UvisClient/controllers/HomeController.java
@@ -1,14 +1,22 @@
 package com.uniovi.UvisClient.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.uniovi.UvisClient.UvisClientApplication;
+import com.uniovi.UvisClient.communication.BlockChainSessionHandler;
 import com.uniovi.UvisClient.entities.BlockChain;
+import com.uniovi.UvisClient.entities.dto.BlockChainDto;
+import com.uniovi.UvisClient.services.impl.BlockChainServiceImpl;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private BlockChainServiceImpl chainService;
 
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String home(Model model) {
@@ -17,4 +25,18 @@ public class HomeController {
 		model.addAttribute("transactionList", BlockChain.getInstance().getTransactionsList());
 		return "home";
 	}
+	
+	@RequestMapping("/")
+	public String getIndex(Model model) {
+		return "redirect:login";
+	}
+	
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		this.chainService.send(new BlockChainDto(), UvisClientApplication.initNode.getUrl(), new BlockChainSessionHandler(), "/app/chain/sendChain");
+		return "login";
+	}
+	
+	
 }
--- src/main/java/com/uniovi/UvisClient/controllers/UserController.java
+++ src/main/java/com/uniovi/UvisClient/controllers/UserController.java
@@ -8,12 +8,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.uniovi.UvisClient.UvisClientApplication;
-import com.uniovi.UvisClient.communication.BlockChainSessionHandler;
 import com.uniovi.UvisClient.entities.User;
-import com.uniovi.UvisClient.entities.dto.BlockChainDto;
 import com.uniovi.UvisClient.services.UserService;
-import com.uniovi.UvisClient.services.impl.BlockChainServiceImpl;
 import com.uniovi.UvisClient.services.security.SecurityService;
 import com.uniovi.UvisClient.validator.SignUpFormValidator;
 
@@ -26,27 +22,13 @@ public class UserController {
 	@Autowired
 	private SecurityService securityService;
 	
-	@Autowired
-	private BlockChainServiceImpl chainService;
-	
 	@Autowired 
 	private SignUpFormValidator signUpFormValidator;
-
-	@RequestMapping("/")
-	public String getIndex(Model model) {
-		return "redirect:login";
-	}
 
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
 		model.addAttribute("usersList", usersService.getUsers());
 		return "user/list";
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(Model model) {
-		this.chainService.send(new BlockChainDto(), UvisClientApplication.initNode.getUrl(), new BlockChainSessionHandler(), "/app/chain/sendChain");
-		return "login";
 	}
 	
 	@RequestMapping(value = { "/signup" }, method = RequestMethod.GET)
