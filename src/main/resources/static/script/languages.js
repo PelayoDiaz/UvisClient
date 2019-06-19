cc451fc8b1503248d9f0ddcb7705fdb090db2a81
Pelayo Díaz Soto <UO251000@uniovi.es>
2019-06-19 23:01:00 +0200

    Incluido Cambio de idioma en login

--- src/main/resources/static/script/languages.js
+++ src/main/resources/static/script/languages.js
@@ -8,3 +8,21 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+$(document).ready(function() {
+	$("#btnEnglish").click(function(e) {
+		e.preventDefault(); // cancel the link behaviour
+		var languageSelectedValue = $(this).attr("value");
+		window.location.replace('?lang=' + languageSelectedValue);
+		return false;
+	});
+});
+
+$(document).ready(function() {
+	$("#btnSpanish").click(function(e) {
+		e.preventDefault(); // cancel the link behaviour
+		var languageSelectedValue = $(this).attr("value");
+		window.location.replace('?lang=' + languageSelectedValue);
+		return false;
+	});
+});
